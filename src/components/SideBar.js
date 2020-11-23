e2a366487e26bcc23405b28be7dcf865f8238540
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-24 04:57:05 +0500

    update SideBar.js

---
+++ src/components/SideBar.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const SideBar = () => {
+  const [state, setState] = useState([
+    { id: 1, image: "/images/umair1.jpg", name: "Flutter Development" },
+    { id: 2, image: "/images/umair2.jpg", name: "PHP Development" },
+    { id: 3, image: "/images/umair3.jpg", name: "React Native Development" },
+    { id: 4, image: "/images/talha1.jpg", name: "Node JS Development" },
+    { id: 5, image: "/images/hamza1.jpg", name: "Vue JS Development" },
+    { id: 6, image: "/images/dawood.jpg", name: "React Development" },
+  ]);
+  return (
+    <div className="sidebar">
+      {state.map((info) => (
+        <div className="sidebar__list" key={info.id}>
+          <div className="sidebar__list-img">
+            <img src={info.image} alt="group image" />
+          </div>
+          <div className="sidebar__list-name">{info.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SideBar;

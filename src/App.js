f506f141587237404ca104b14cb7e7d4be7c1ee4
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-24 05:10:53 +0500

    update App.js import Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import Navbar from "./components/Navbar";
+import SideBar from "./components/SideBar";
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="facebook">
+        <SideBar />
+        <Posts />
+        <ChatBar openChat={openChat} />
+        <ChatBox state={state} current={current} closeChat={closeChat} />
+      </div>
+    </div>
+  )
  
 }
 

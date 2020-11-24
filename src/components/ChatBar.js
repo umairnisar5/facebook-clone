316805695dc1f08165048f9d4801f604ceba4369
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-24 17:34:53 +0500

    adding picture

--- src/components/ChatBar.js
+++ src/components/ChatBar.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 const ChatBar = (props) => {
-//   const [state, setState] = React.useState([
-//     { id: 1, image: "/images/fawad1.jpg", name: "Fawad Khan" },
-//     { id: 2, image: "/images/fawad2.jpg", name: "Aasad Khan" },
-//     { id: 3, image: "/images/fawad3.jpg", name: "Kamran Khan" },
-//     { id: 4, image: "/images/fawad4.jpg", name: "Javed Khan" },
-//     { id: 5, image: "/images/imran1.jpg", name: "Fazal Khan" },
-//     { id: 6, image: "/images/imran2.jpg", name: "Rahim Khan" },
-//     { id: 6, image: "/images/imran3.jpg", name: "Imran Khan" },
-//     { id: 7, image: "/images/profile.jfif", name: "Saeed Khan" },
-//     { id: 8, image: "/images/fawad1.jpg", name: "Bilal Khan" },
-//     { id: 9, image: "/images/fawad2.jpg", name: "Umer Khan" },
-//     { id: 10, image: "/images/fawad3.jpg", name: "Usman Khan" },
-//   ]);
-//   const openChat = (user) => {
-//     props.openChat(user);
-//   };
+  const [state, setState] = React.useState([
+    { id: 1, image: "/images/umair1.jpg", name: "Umair Nisar" },
+    { id: 2, image: "/images/umair2.jpg", name: "Aasad Khan" },
+    { id: 3, image: "/images/umair3.jpg", name: "Kamran Khan" },
+    { id: 4, image: "/images/fawad4.jpg", name: "Javed Khan" },
+    { id: 5, image: "/images/talha1.jpg", name: "Fazal Khan" },
+    { id: 6, image: "/images/hamza1.jpg", name: "Rahim Khan" },
+    { id: 6, image: "/images/imran3.jpg", name: "Imran Khan" },
+    { id: 7, image: "/images/profile.jpg", name: "Saeed Khan" },
+    { id: 8, image: "/images/fawad1.jpg", name: "Bilal Khan" },
+    { id: 9, image: "/images/fawad2.jpg", name: "Umer Khan" },
+    { id: 10, image: "/images/fawad3.jpg", name: "Usman Khan" },
+  ]);
+  const openChat = (user) => {
+    props.openChat(user);
+  };
   return (
     <div className="chatBar">
       {state.map((user) => (
